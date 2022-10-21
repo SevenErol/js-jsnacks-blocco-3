821f2da6b3e8ffb854911450adaeb572529dbbe2
SevenErol <lorenzo.maggi1998@gmail.com>
2022-10-21 11:13:26 +0200

    initial commit created array

---
+++ js-snack-1/assets/javascript/script.js
@@ -0,0 +1,97 @@
+
+// Crea un array composto da 10 automobili.
+// Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+// Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// Infine stampa separatamente i 3 array.
+
+
+const carsList = [
+    {
+        marca: "Opel",
+
+        modello: "Corsa",
+
+        alimentazione: "Benzina"
+    },
+
+    {
+        marca: "Fiat",
+
+        modello: "Panda",
+
+        alimentazione: "Benzina"
+    },
+
+    {
+        marca: "Volvo",
+
+        modello: "XC40",
+
+        alimentazione: "Diesel"
+    },
+
+    {
+        marca: "Citroen",
+
+        modello: "C4",
+
+        alimentazione: "Diesel"
+    },
+
+    {
+        marca: "Opel",
+
+        modello: "Astra",
+
+        alimentazione: "Diesel"
+    },
+
+    {
+        marca: "Peugeot",
+
+        modello: "208",
+
+        alimentazione: "Benzina"
+    },
+
+    {
+        marca: "Peugeot",
+
+        modello: "3008",
+
+        alimentazione: "Diesel"
+    },
+
+    {
+        marca: "Mini",
+
+        modello: "Cooper",
+
+        alimentazione: "Benzina"
+    },
+
+    {
+        marca: "Tesla",
+
+        modello: "Model s",
+
+        alimentazione: "Elettrica"
+    },
+
+    {
+        marca: "Toyota",
+
+        modello: "Yaris",
+
+        alimentazione: "Ibrida"
+    },
+
+    {
+        marca: "Toyota",
+
+        modello: "Corolla",
+
+        alimentazione: "Ibrida"
+    },
+
+];
